a22e7f836876c1b8d3a620263522331cbeae7f30
risingisland <risingisland@yahoo.com>
2026-03-15 22:24:54 +0000

    v1.0
    add langs

---
+++ Dashboard/modules/tinyweather/lang/nb_NO.php
@@ -0,0 +1,34 @@
+<?php
+
+$i18n = [
+
+	'tw_lang_0_Clear'		=>	'Klarvær',
+	'tw_lang_1_Mainly'		=>	'For det meste klart',
+	'tw_lang_2_Partly'		=>	'Delvis skyet',
+	'tw_lang_3_Overcast'	=>	'Skyet',
+	'tw_lang_45_Fog'		=>	'Tåke',
+	'tw_lang_48_Rime'		=>	'Rimtåke',
+	'tw_lang_51_Light'		=>	'Lett yr',
+	'tw_lang_53_Moderate'	=>	'Moderat yr',
+	'tw_lang_55_Dense'		=>	'Tett yr',
+	'tw_lang_56_Freezing'	=>	'Underkjølt yr',
+	'tw_lang_57_Dense'		=>	'Tett underkjølt yr',
+	'tw_lang_61_Slight'		=>	'Lett regn',
+	'tw_lang_63_Moderate'	=>	'Moderat regn',
+	'tw_lang_65_Heavy'		=>	'Kraftig regn',
+	'tw_lang_66_Freezing'	=>	'Underkjølt regn',
+	'tw_lang_67_Heavy'		=>	'Kraftig underkjølt regn',
+	'tw_lang_71_Slight'		=>	'Lett snø',
+	'tw_lang_73_Moderate'	=>	'Moderat snø',
+	'tw_lang_75_Heavy'		=>	'Kraftig snø',
+	'tw_lang_77_Snow'		=>	'Snøkorn',
+	'tw_lang_80_Slight'		=>	'Lette regnbyger',
+	'tw_lang_81_Moderate'	=>	'Moderate regnbyger',
+	'tw_lang_82_Violent'	=>	'Kraftige regnbyger',
+	'tw_lang_85_Slight'		=>	'Lette snøbyger',
+	'tw_lang_86_Heavy'		=>	'Kraftige snøbyger',
+	'tw_lang_95_Thunderstorm'	=>	'Torden',
+	'tw_lang_96_Thunderstorm'	=>	'Torden med hagl',
+	'tw_lang_99_Thunderstorm'	=>	'Torden med kraftig hagl',
+	
+];
